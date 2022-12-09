7b2722b44de4c05a1373f2d09bc4131aba16b885
Luis Henrique <luish.fa1515@gmail.com>
2022-12-09 17:53:54 -0300

    Info_project function done.

---
+++ src/services/InfoProjectService.ts
@@ -0,0 +1,41 @@
+import { prisma } from "../prisma/prismaClient";
+import { createError } from "../utils/createError";
+
+class InfoProjectService {
+	async execute({ id, path }) {
+		let where = {};
+
+		if(id) {
+			where = { id };
+		} else {
+			where = { path };
+		}
+
+		const project = await prisma.project.findFirst({
+			where
+		});
+
+		if(!project) {
+			return createError("Project not found.");
+		}
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: "Project found!",
+				project: {
+					id: project.id,
+					owner_id: project.owner,
+					title: project.title,
+					description: project.description,
+					path: project.path,
+					platform: project.platform,
+					link: project.link,
+					tags: project.tags,
+				}
+			})
+		};
+	}
+}
+
+export { InfoProjectService };
